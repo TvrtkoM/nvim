fc2709ebbeab45f98dbf9d186025fedea088dfeb
Tvrtko Majstorović <tvrtkomaj@gmail.com>
2026-06-26 05:51:27 +0200

    update gitsigns keybindings

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -21,8 +21,8 @@ return {
       map("n", "<leader>gs", gs.stage_hunk, "Stage hunk")
       map("v", "<leader>gs", function() gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, "Stage selection")
       map("n", "<leader>gr", gs.reset_hunk, "Reset hunk")
-      map("n", "<leader>gu", gs.undo_stage_hunk, "Undo stage hunk")
-      map("n", "<leader>gp", gs.preview_hunk, "Preview hunk")
+      map("n", "<leader>gu", gs.stage_hunk, "Unstage hunk")
+      map("n", "<leader>gp", gs.preview_hunk_inline, "Preview hunk")
       map("n", "<leader>gb", function() gs.blame_line({ full = true }) end, "Blame line")
       map("n", "<leader>gd", gs.diffthis, "Diff this file")
     end,
